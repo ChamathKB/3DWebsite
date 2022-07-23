84dcda5893bdb4095eeebb99ae01cf17e739b197
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-07-24 00:52:24 +0530

    added new components

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 3D Web site with threejs
+
+3D Portfolio website using threejs.
+
+based on a [fireship](https://www.youtube.com/watch?v=Q7AOvWpIVHU) tutorial 🤝
--- main.js
+++ main.js
@@ -22,7 +22,7 @@ renderer.render(scene, camera);
 
 // torus
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00004d });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
@@ -38,12 +38,12 @@ scene.add(pointLight, ambientLight);
 
 
 // helpers
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
 
-scene.add(lightHelper, gridHelper)
+// scene.add(lightHelper, gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 
 // add random items
@@ -75,7 +75,43 @@ scene.add(chamath);
 
 
 // moon
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon)
+
+moon.position.z = 30;
+moon.position.setX(-10); //moon.position.x = -10;
+
+chamath.position.z = -5;
+chamath.position.z = 2;
+
+
+// scroll animation
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  chamath.rotation.y += 0.01;
+  chamath.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 // animation Loop
 function animate() {
